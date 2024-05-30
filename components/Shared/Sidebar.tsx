05d9ab73032095d885e8a6250cf254b384a8246f
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-05-30 16:51:03 -0300

    more

--- components/Shared/Sidebar.tsx
+++ components/Shared/Sidebar.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { SignedIn } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { navLinks } from '../constants'
 import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button';
 
 function Sidebar(){
     const pathname = usePathname();
@@ -23,12 +24,27 @@ function Sidebar(){
                                 const isActive = link.route === pathname
                                 return (
                                     <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
-                                        {link.label}
+                                        <Link className='sidebar-link' href={link.route}>
+                                            <Image src={link.icon} 
+                                            alt="logo"
+                                            width={24}
+                                            height={24}
+                                            className={`${isActive && 'brightness-200'}`} />
+                                            {link.label}
+                                        </Link>
                                     </li>
                                 )
                             })}
+                            <li className="flex-center cursor-pointer gap-2 p-4">
+                                <UserButton afterSignOutUrl='/' showName />
+                            </li>
                         </ul>
                     </SignedIn>
+                    <SignedOut>
+                        <Button asChild className='button bg-purple-gradient bg-cover'>
+                            <Link href="/sign-in"></Link>
+                        </Button>
+                    </SignedOut>
                 </nav>
             </div>
         </aside>
