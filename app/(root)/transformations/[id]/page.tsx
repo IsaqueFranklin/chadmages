79838bcac38d3ff6d8bd6587000dad63192f28ef
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-05-30 16:41:05 -0300

    first commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TransformationsPage() {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+//localhost:3001/transformartions/[id]
