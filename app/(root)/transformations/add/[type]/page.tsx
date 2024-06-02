6ad0c46eec44a137af0c4c72e6a8241f3cdcce70
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-06-02 19:57:22 -0300

    continuing the form component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,10 +2,19 @@ import Header from '@/components/Shared/Header'
 import React from 'react'
 import { transformationTypes } from '@/constants'
 import TransformationForm from '@/components/Shared/TransformationForm';
+import { auth } from '@clerk/nextjs/server';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
-function AddTransformationTypePage({ params: { type }}: SearchParamProps) {
+async function AddTransformationTypePage({ params: { type }}: SearchParamProps) {
     
+  const {userId} = auth();
+
   const transformation = transformationTypes[type];
+
+  if(!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId)
   return (
     <>
       <Header 
@@ -13,7 +22,12 @@ function AddTransformationTypePage({ params: { type }}: SearchParamProps) {
       subtitle={transformation.subTitle} 
       />
 
-      <TransformationForm />
+      <TransformationForm 
+      action="Add" 
+      userId={user?._id} 
+      type={transformation.type as TransformationTypeKey} 
+      creditBalance={user?.creditBalance}
+      />
     </>
   )
 }
--- components/Shared/TransformationForm.tsx
+++ components/Shared/TransformationForm.tsx
@@ -3,6 +3,14 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form";
 
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"  
+
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -14,9 +22,15 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { defaultValues, transformationTypes } from "@/constants";
+import { CustomField } from "./CustomField";
 
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
+export const formSchema = z.object({
+    title: z.string(),
+    aspectRatio: z.string().optional(),
+    color: z.string().optional(),
+    prompt: z.string().optional(),
+    publicId: z.string(),
 })
 
 function onSubmit(values: z.infer<typeof formSchema>) {
@@ -25,35 +39,51 @@ function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
   }
 
-const TransformationForm = () => {
+const TransformationForm = ({ action, data = null, userId, type, creditBalance }: TransformationFormProps) => {
+
+    const transformationType = transformationTypes[type];
+
+    const initialValues = data && action === "Update" ? {
+        title: data?.title,
+        aspectRatio: data?.aspectRatio,
+        color: data?.color,
+        prompt: data?.prompt,
+        publicId: data?.publicId,
+    } : defaultValues
 
     const form = useForm<z.infer<typeof formSchema>>({
+
         resolver: zodResolver(formSchema),
-        defaultValues: {
-          username: "",
-        },
+        defaultValues: initialValues,
       })
 
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Username</FormLabel>
-                        <FormControl>
-                        <Input placeholder="shadcn" {...field} />
-                        </FormControl>
-                        <FormDescription>
-                        This is your public display name.
-                        </FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                    )}
+                <CustomField 
+                control={form.control} 
+                name="title" 
+                formLabel="Image Title" 
+                className="w-full" 
+                render={({ field }) => <Input {...field} className="input-field" />} 
                 />
-                <Button type="submit">Submit</Button>
+
+                {type === "fill" && (
+                    <CustomField
+                    render={({ field }) => (
+                        <Select>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Theme" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="light">Light</SelectItem>
+                            <SelectItem value="dark">Dark</SelectItem>
+                            <SelectItem value="system">System</SelectItem>
+                        </SelectContent>
+                        </Select>
+
+                    )} />
+                )}
             </form>
         </Form>
     )
